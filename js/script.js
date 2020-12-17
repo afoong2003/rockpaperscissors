752b85037cc375100b57e156ebd14c017b83050b
Glitch (steadfast-vast-freon) <none>
2020-12-17 23:55:08 +0000

    day 2

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-
-
-
+$("#shoot").click(function() {
+  let userInput = $("#input").val();
+  $("#userChoice").text(userInput);
+  let computerDecision = $("#computerChoice").;
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  $("#computerChoice").text(randomNumber);
+  
+  if (randomNumber === 1) {
+    $("#computerChoice").text("Rock");
+  } else if (randomNumber === 2) {
+    $("#computerChoice").text("Paper");
+  } else if (randomNumber === 3) {
+    $("#computerChoice").text("Scissors");
+  }
+  if (userInput === computerDecision) {
+    $("#result").text("Tie");
+    
+    
+    
+    
+    
+    
+  }
+});
